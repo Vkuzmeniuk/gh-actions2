4c16bd2f1442b95790fe82769f8a35aeae82cb58
vkuzmeniuk <vladislav.kuzmeniuk@gmail.com>
2024-04-19 02:13:41 +0200

    Updated output information

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,10 +9,12 @@ function run() {
   const distFolder = core.getInput("dist-folder", { required: true });
 
   // Upload files
-  const s3Uri = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
   core.notice("Hello from my custom JavaScriptAction!");
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
